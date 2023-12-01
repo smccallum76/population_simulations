f21c57f65cdcc6ca3203a2cdc16c486ba62aaf7f
smccallum76 <scott.mccallum.42@gmail.com>
2023-12-01 11:48:33 -0500

    Updated plots to included iHS standardization.

--- statistics/01_simulation_statistics.py
+++ statistics/01_simulation_statistics.py
@@ -31,6 +31,25 @@ ac_eur = gt_sweep_EUR.count_alleles()
 ac_afrN = gt_neutral_AFR.count_alleles()
 ac_eurN = gt_neutral_EUR.count_alleles()
 '''
+Plot of allele count -- qc to ensure that we are seeing a bunch of 1's near the sweep
+'''
+allel_pos = 1
+fig, ax = plt.subplots(nrows=2, ncols=1, sharey=False, sharex=True, figsize=(18, 8))
+ax[0].plot(sweep['variants/POS'], ac_afr[:, allel_pos], label="AFR ac Sweep", alpha=0.7)
+ax[0].plot(sweep['variants/POS'], ac_eur[:, allel_pos]*-1, label="EUR ac Sweep", alpha=0.7, color='red')
+ax[1].plot(neutral['variants/POS'], ac_afrN[:, allel_pos], label="AFR ac Neutral", alpha=0.7)
+ax[1].plot(neutral['variants/POS'], ac_eurN[:, allel_pos]*-1, label="EUR ac Neutral", alpha=0.7, color='red')
+ax[0].axvline(x=np.max(sweep['variants/POS'])/2, color='black', linestyle='--', label='Sweep Location')
+ax[0].set_xlabel("SNP Number")
+ax[0].set_title("Sweep Sims -- Allele Frequency by SNP")
+ax[0].legend(loc="upper left")
+ax[1].set_xlabel("SNP Number")
+ax[1].axvline(x=np.max(sweep['variants/POS'])/2, color='black', linestyle='--', label='Sweep Location')
+ax[1].set_title("Neutral Sims -- Allele Frequency by SNP")
+ax[1].legend(loc="upper left")
+plt.show()
+
+'''
 XP-EHH
 - Should be comparing AFR to EUR (these are the two populations)
 - The VCF file should be sliced according to the positions of the AFR and EUR populations
@@ -70,7 +89,7 @@ ax[1].set_title('Histogram of XP-EHH Neutral')
 plt.show()
 
 '''
-iHS
+iHS - Unstandardized
 - it is normal that iHS returns a lot of nans
 - this should only be applied to one population at a time (AFR and then EUR)
 '''
@@ -79,26 +98,37 @@ ihs_eur = sa.ihs(sweep['calldata/GT'][:, eur_s:eur_e, 0], pos=sweep['variants/PO
 ihs_afrN = sa.ihs(neutral['calldata/GT'][:, afr_s:afr_e, 0], pos=neutral['variants/POS'], include_edges=False, use_threads=True)
 ihs_eurN = sa.ihs(neutral['calldata/GT'][:, eur_s:eur_e, 0], pos=neutral['variants/POS'], include_edges=False, use_threads=True)
 
-'''Genotype Position Plots - iHS'''
+# Standardize the iHS scores
+ihs_afr_std, ac_afr_bins = sa.standardize_by_allele_count(ihs_afr, ac_afr[:, 1], diagnostics=False)
+ihs_eur_std, ac_eur_bins = sa.standardize_by_allele_count(ihs_eur, ac_eur[:, 1], diagnostics=False)
+ihs_afrN_std, ac_afrN_bins = sa.standardize_by_allele_count(ihs_afrN, ac_afrN[:, 1], diagnostics=False)
+ihs_eurN_std, ac_eurN_bins = sa.standardize_by_allele_count(ihs_eurN, ac_eurN[:, 1], diagnostics=False)
 
-plt.figure(figsize=(18, 5))
-plt.scatter(np.arange(0, len(ihs_afr), 1), ihs_afr, label='AFR', s=3)
-plt.scatter(np.arange(0, len(ihs_eur), 1), ihs_eur, label='EUR', s=3, alpha=0.3, color='red')
-plt.axhline(y=0, color='black')
-plt.axvline(x=len(ihs_afr)/2, color='black', linestyle='--', label="Sweep Location")
-plt.xlabel("Genomic Position")
-plt.ylabel("iHS")
-plt.title("iHS using AFR and EUR populations")
-plt.legend(loc="upper left")
+'''SNP Position Plots - iHS'''
+fig, ax = plt.subplots(nrows=2, ncols=1, sharey=True, figsize=(15, 8))
+ax[0].scatter(sweep['variants/POS'], abs(ihs_afr_std), label='AFR Sweep', s=3, color='blue')
+ax[0].scatter(sweep['variants/POS'], abs(ihs_eur_std)*-1, label='EUR Sweep', s=3, color='red')
+ax[0].axvline(x=np.max(sweep['variants/POS'])/2, color='black', linestyle='--', label="Sweep Location")
+ax[0].set_ylabel("|iHS| or |iHS|*-1")
+ax[0].set_title("Sweep Sims -- iHS using AFR and EUR populations")
+ax[0].legend(loc="upper left")
+
+ax[1].scatter(neutral['variants/POS'], abs(ihs_afrN_std), label='AFR Neutral', s=3, color='blue')
+ax[1].scatter(neutral['variants/POS'], abs(ihs_eurN_std)*-1, label='EUR Neutral', s=3, color='red')
+ax[1].axvline(x=np.max(sweep['variants/POS'])/2, color='black', linestyle='--', label="Sweep Location")
+ax[1].set_xlabel("SNP Number")
+ax[1].set_ylabel("|iHS| or |iHS|*-1")
+ax[1].set_title("Neutral Sims -- iHS using AFR and EUR populations")
+ax[1].legend(loc="upper left")
 plt.show()
 
 '''Histogram Plots - iHS '''
 n_bins = 100
 fig, ax = plt.subplots(nrows=2, ncols=2, sharey=True, figsize=(12, 8))
-ax[0, 0].hist(ihs_afr, bins=n_bins, color='blue')
-ax[0, 1].hist(ihs_eur, bins=n_bins, color='blue')
-ax[1, 0].hist(ihs_afrN, bins=n_bins, color='red')
-ax[1, 1].hist(ihs_eurN, bins=n_bins, color='red')
+ax[0, 0].hist(ihs_afr_std, bins=n_bins, color='blue')
+ax[0, 1].hist(ihs_eur_std, bins=n_bins, color='blue')
+ax[1, 0].hist(ihs_afrN_std, bins=n_bins, color='red')
+ax[1, 1].hist(ihs_eurN_std, bins=n_bins, color='red')
 ax[1, 0].set_xlabel('iHS Value')
 ax[0, 0].set_title('Histogram of iHS AFR Sweep')
 ax[0, 1].set_title('Histogram of iHS EUR with Sweep in AFR')
