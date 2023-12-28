b4bc697826114ed0b7a70da492855fbe354bccd9
scottmccallum76 <scott.mccallum.42@gmail.com>
2023-12-28 11:40:04 -0500

    notes added to test script.

--- db_build/sqlite_test.py
+++ db_build/sqlite_test.py
@@ -6,6 +6,7 @@ import sqlite3
 from sqlalchemy import create_engine
 import pandas as pd
 import allel as sa
+import numpy as np
 
 '''
 Note about location of the hard sweep:
@@ -15,16 +16,29 @@ Note about location of the hard sweep:
 - This location should exist at every single Sweep simulation, but will not necessarily exist at every
 neutral location (in most cases it will not). 
 - Therefore,  the stats for the  sweep should correspond to variants/POS  == 2,500,000 in all cases)
-- The linkage should be bumpded x-base pairs left and right from this position
+- The linkage should be bumped x-base pairs left and right from this position
 - Everything else is neutral. 
 '''
 
 sweep = sa.read_vcf("vcf_files_temp/sweep/ts_sweep_5.vcf")
+gt_sweep = sa.GenotypeArray(sweep['calldata/GT'])
 
-# engine = sqlalchemy.create_engine('sqlite:///test.db', echo=False)
-# df = pd.DataFrame([[1,2],[1,2]], columns=['a', 'b'])
-#
-# df.to_sql('mytable', con=engine, if_exists='append')
-#
+# if data is brought in as a numpy array and then converted to a GenotypeArray then it appears that the functionality
+# of scikit-allel is available. Therefore, if the data is sent to a DB as a 2D matrix then when it is brought back into
+# a script for stat analysis THEN be sure to honor the expected shape of the needed array. Specifically, keep the
+# Genotype array as a 3D matrix (i.e., reshape). Copied below is a basic example of bringing in numpy data and then
+# flipping it to a Genotypearray.
+gt_test = np.array(gt_sweep)
+gt_test2 = sa.GenotypeArray(gt_test)
+
+# allele counts at each position
+ac_afr = gt_sweep.count_alleles()
+ac_test = gt_test2.count_alleles() # same answer as ac_afr
+
+
+
+
+# engine = create_engine('sqlite:///test.db', echo=False)
+# sweep.to_sql('sweep_test', con=engine, if_exists='append')
 # conn = sqlite3.connect('test.db')
 print('done')
