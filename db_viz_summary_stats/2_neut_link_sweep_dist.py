3291ff1b8f906664c7a33dabd650c9dab04ada7b
scottmccallum76 <scott.mccallum.42@gmail.com>
2024-01-21 19:06:39 -0500

    minor updates

--- db_viz_summary_stats/2_neut_link_sweep_dist.py
+++ db_viz_summary_stats/2_neut_link_sweep_dist.py
@@ -14,7 +14,7 @@ Query the simulation db to determine the unique number of simulations
 '''
 start = time.time()
 path = 'C:/Users/scott/PycharmProjects/population_simulations/db_build/'
-conn = sqlite3.connect(path + 'population_simulation.db')
+conn = sqlite3.connect(path + 'population_simulation_v2.db')
 # enter the stats table name (neutral or sweep)
 table_name = 'sweep_simulations_stats'
 
