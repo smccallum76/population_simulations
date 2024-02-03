69e0fe5f1005e2d5fb6c143e9571d677d17d7b9a
scottmccallum76 <scott.mccallum.42@gmail.com>
2024-02-03 11:34:05 -0500

    Add code to export test data to be used post-swifr train.

--- SWIFr_data_prep/1_swifr_data_prep_train.py
+++ SWIFr_data_prep/1_swifr_data_prep_train.py
@@ -20,6 +20,8 @@ path = 'C:/Users/scott/PycharmProjects/population_simulations/db_build/'
 conn = sqlite3.connect(path + 'population_simulation_v2.db')
 # enter the stats table name (neutral or sweep)
 table_name = 'sweep_simulations_stats'
+test_count = 20  # number of simulations to use in the test data for swifr
+vcf_names = tuple(['ts_sweep_' + str(i) + '.vcf' for i in range(test_count)])  # list of vcf files to include in test
 
 sql1 = (f"""
        SELECT
@@ -57,12 +59,28 @@ sql3 = (f"""
         WHERE label IS 'neutral'
        """)
 
+sql4 = (f"""
+       SELECT
+        label,
+        vcf_name,
+        snp_position,
+        xpehh,
+        fst,
+        ihs_afr_std
+
+        FROM {table_name}
+        WHERE vcf_name in {vcf_names}
+       """)
+
 # collect a list of the unique simulations
 sweep = pd.read_sql(sql1, conn)
 link = pd.read_sql(sql2, conn)
 neutral = pd.read_sql(sql3, conn)
+test = pd.read_sql(sql4, conn)
+
 # there are ~22 million rows of neutral data, for now I'm knocking this back to 3 million b/c that's just excessive
 neutral = neutral.iloc[0:3000000, :]
+
 
 ''' Print the pct of nans in ihs data'''
 nans_sweep = (sweep['ihs_afr_std'].isna().sum()) / len(sweep['ihs_afr_std'])
@@ -81,5 +99,6 @@ neutral = neutral.fillna(-998)
 link.to_csv('simulations_4_swifr/link/link', sep='\t', index=False)
 neutral.to_csv('simulations_4_swifr/neutral/neutral', sep='\t', index=False)
 sweep.to_csv('simulations_4_swifr/sweep/sweep', sep='\t', index=False)
+test.to_csv('simulations_4_swifr/test/test', sep='\t', index=False)
 
 print('done')
